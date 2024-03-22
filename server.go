684beb7e76a29783a972909d05d3c7c678367e6e
paulovigne <57726813+paulovigne@users.noreply.github.com>
2024-03-22 20:06:04 -0300

    Update server.go

--- server.go
+++ server.go
@@ -6,7 +6,7 @@ import (
 )
 
 func helloWorld(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Hellow World 22 março 2024")
+    fmt.Fprintf(w, "Hellow World 22 março 2024!")
 }
 
 func main() {
